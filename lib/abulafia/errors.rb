acfdca987d99cb29c4ff1fefa81f09e4542c0467
dan sinclair <dj2@everburning.com>
2021-09-03 08:06:03 -0400

    Update error handling

--- lib/abulafia/errors.rb
+++ lib/abulafia/errors.rb
@@ -3,29 +3,39 @@
 module Abulafia
   # Error raised when given command name isn't found.
   class InvalidCommand < RuntimeError
+    def message
+      'invalid Abulafia command'
+    end
   end
 
   # Error raised if multiple commands are registered with the same name.
   class DuplicateCommandName < RuntimeError
-    attr_accessor :name
-
     def initialize(name)
       super
-      self.name = name
+      @name = name
+    end
+
+    def message
+      "duplicate command registered: #{@name}"
     end
   end
 
   # Error raised if note name was not provided.
   class MissingFile < RuntimeError
+    def message
+      'note name required'
+    end
   end
 
   # Error raised if requested note can not be found.
   class NoteDoesNotExist < RuntimeError
-    attr_accessor :name
-
     def initialize(name)
       super
-      self.name = name
+      @name = name
+    end
+
+    def message
+      "unable to find note: #{@name}"
     end
   end
 end
